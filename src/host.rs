26b063f31ea9e40a17360d7234be929832eaa36c
Thomas Q <thomasqsa@gmail.com>
2026-08-20 00:44:49 -0300

    Add missing interrupt pipes

--- src/host.rs
+++ src/host.rs
@@ -71,6 +71,34 @@ pub async fn host_task(mut host: HostDriver<'static, USB_OTG_FS, 12>) {
 
                 defmt::info!("Dongle configured! Entering passthrough loop...");
 
+                let mut int_out_pipe = host.inner.allocator().alloc_pipe::<
+                    embassy_usb_driver::host::pipe::Interrupt,
+                    embassy_usb_driver::host::pipe::Out
+                >(
+                    1,
+                    &embassy_usb_driver::EndpointInfo {
+                        addr: embassy_usb_driver::EndpointAddress::from_parts(3, embassy_usb_driver::Direction::Out),
+                        ep_type: embassy_usb_driver::EndpointType::Interrupt,
+                        max_packet_size: 64,
+                        interval_ms: 1,
+                    },
+                    None,
+                ).unwrap();
+
+                let mut int_in_pipe = host.inner.allocator().alloc_pipe::<
+                    embassy_usb_driver::host::pipe::Interrupt,
+                    embassy_usb_driver::host::pipe::In
+                >(
+                    1,
+                    &embassy_usb_driver::EndpointInfo {
+                        addr: embassy_usb_driver::EndpointAddress::from_parts(4, embassy_usb_driver::Direction::In),
+                        ep_type: embassy_usb_driver::EndpointType::Interrupt,
+                        max_packet_size: 64,
+                        interval_ms: 1,
+                    },
+                    None,
+                ).unwrap();
+
                 let mut nonce_buf = [0u8; 63];
                 let mut state_buf = [0u8; 15];
 
@@ -102,13 +130,38 @@ pub async fn host_task(mut host: HostDriver<'static, USB_OTG_FS, 12>) {
                         _ => {}
                     }
 
-                    // Handle Gamepad Hashing
-                    if let Ok(_report) = HASH_REQ_CHANNEL.try_receive() {
-                        // TODO: Use Interrupt IN/OUT pipes to hash `_report` against the Dongle
+                    // Handle Gamepad Report Hashing using the new Interrupt Pipes
+                    if let Ok(report) = HASH_REQ_CHANNEL.try_receive() {
+                        let mut buf = [0u8; 64];
 
-                        // For now, feed a dummy hash back so the PS5 main loop doesn't block!
-                        let dummy_hash = [0x00; 8];
-                        let _ = HASH_RES_CHANNEL.try_send(dummy_hash);
+                        // Serialize the report struct into bytes
+                        unsafe {
+                            core::ptr::copy_nonoverlapping(
+                                &report as *const _ as *const u8,
+                                buf.as_mut_ptr(),
+                                64,
+                            );
+                        }
+
+                        // Write to Dongle (the second parameter is `ensure_transaction_end`, we can set it to false here)
+                        if let Err(e) = int_out_pipe.request_out(&buf, false).await {
+                            defmt::error!("Interrupt OUT Error: {:?}", e);
+                            break; // Pipe broken, exit loop
+                        }
+
+                        // Immediately read the hashed report back
+                        let mut in_buf = [0u8; 64];
+                        if let Err(e) = int_in_pipe.request_in(&mut in_buf).await {
+                            defmt::error!("Interrupt IN Error: {:?}", e);
+                            break;
+                        }
+
+                        // The last 8 bytes are the crypto hash signature
+                        let mut real_hash = [0u8; 8];
+                        real_hash.copy_from_slice(&in_buf[56..64]);
+
+                        // Feed it back to the PS5 task
+                        let _ = HASH_RES_CHANNEL.try_send(real_hash);
                     }
 
                     Timer::after(Duration::from_millis(5)).await;
